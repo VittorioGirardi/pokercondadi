289d51f802956a6509504a43e8503e5087fe793d
Vittorio <girardi.vittorio@live.it>
2023-04-18 14:33:21 +0200

    Test poker con dadi

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Random random = new Random();
+
+int points = 0;
+int count = 0;
+
+List<int> dadi = new List<int>();
+
+while (count != 10)
+{
+    Console.WriteLine("Premi un tasto per lanciare i dadi.");
+    Console.ReadLine();
+    dadi.Add(random.Next(0, 6));
+    count = count + 1;
+    foreach (var i in dadi)
+    {
+        string.Join(", ", dadi);
+        Console.Write("|" + i + "|");
+    }
+
+    if (dadi.Contains(5))
+    {
+        Console.WriteLine(" 5 è nella lista.");
+        points = points + 1;
+    }
+    if (dadi.Contains(2))
+    {
+        Console.WriteLine(" 2 è nella lista.");
+        points = points + 1;
+    }
+}
+
